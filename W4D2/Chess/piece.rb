8916f787aa9200939f778c7e1ba2c39d1cb9de0c
Henry <henry-pan@users.noreply.github.com>
2021-02-09 15:30:01 -0800

    Finish Phase I of Chess

--- W4D2/Chess/piece.rb
+++ W4D2/Chess/piece.rb
@@ -1,1 +1,23 @@
+require "singleton"
+
 class Piece
+
+  attr_reader :color, :board, :pos
+
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+  end
+
+end
+
+class NullPiece < Piece
+
+  include Singleton
+
+  def initialize
+    @null_piece = nil
+  end
+
+end
--- W4D2/inheritance.rb
+++ W4D2/inheritance.rb
@@ -1,29 +1,29 @@
 class Employee
-attr_reader :salary
-    def initialize(name, title, salary, boss)
-        @name = name
-        @title = title
-        @salary = salary
-        @boss = boss    
-    end
-   
-    def bonus(multiplier)
-        @salary * multiplier
-    end
+  attr_reader :salary
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss    
+  end
+
+  def bonus(multiplier)
+    @salary * multiplier
+  end
 end
 
 class Manager < Employee
-    attr_writer :sub_employees
-    def initialize(name, title, salary, boss)
-        super
-        @sub_employees = []
-    end
+  attr_writer :sub_employees
+  def initialize(name, title, salary, boss)
+    super
+    @sub_employees = []
+  end
 
-    def bonus(multiplier)
-        sum = 0
-        @sub_employees.each do |employee|
-           sum += employee.salary
-        end
-        sum * multiplier
+  def bonus(multiplier)
+    sum = 0
+    @sub_employees.each do |employee|
+      sum += employee.salary
     end
+    sum * multiplier
+  end
 end
