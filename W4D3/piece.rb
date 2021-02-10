240be8b095ae61686b4a11d31924d8ee5482482d
Henry <henry-pan@users.noreply.github.com>
2021-02-10 10:16:10 -0800

    Move modules into new file, complete Stepable

--- W4D3/piece.rb
+++ W4D3/piece.rb
@@ -1,54 +1,5 @@
 require "singleton"
-
-module Slideable
-  attr_reader :moves
-
-  HORIZONTAL_DIRS = [[0,1],[0,-1],[1,0],[-1,0]]
-  DIAGONAL_DIRS = [[1,1],[-1,-1],[1,-1],[-1,1]]
-
-  def horizontal_dirs
-    HORIZONTAL_DIRS
-  end
-
-  def diagonal_dirs
-    DIAGONAL_DIRS
-  end
-
-  def moves
-    slide_moves = []    
-    move_dirs.each do |dir|
-      dx, dy = dir 
-      slide_moves += grow_unblocked_moves_in_dir(dx, dy)
-    end
-    slide_moves
-  end
-
-  def grow_unblocked_moves_in_dir(dx, dy)
-    unblocked_moves = []
-    current_pos = self.pos
-
-    i = current_pos[0] + dx
-    j = current_pos[1] + dy
-    while i >= 0 && i <= 7 && j >= 0 && j <= 7 
-      if @board[[i, j]].is_a?(NullPiece)  
-        unblocked_moves << [i, j]
-      elsif @board[[i, j]].color != self.color
-        unblocked_moves << [i, j]
-        break
-      else
-        break
-      end
-      i += dx  
-      j += dy
-    end
-    unblocked_moves
-  end
-
-end
-
-module Stepable
-
-end
+require_relative "modules"
 
 class Piece
 
@@ -145,8 +96,9 @@ class Pawn < Piece
     else
       steps << [i, j] if @board[[i, j]].is_a?(NullPiece)
     end
-  end
 
+    steps
+  end
 
   def side_attacks
     steps = []
@@ -155,7 +107,9 @@ class Pawn < Piece
     j = current_pos[1] 
 
     steps << [i, j-1] if j-1 >= 0 && @board[[i, j-1]].color != self.color
-    steps << [i, j+1] if j+1 <= 7 && @board[[i, j+1]].color != self.color 
+    steps << [i, j+1] if j+1 <= 7 && @board[[i, j+1]].color != self.color
+
+    steps
   end
 
 end
