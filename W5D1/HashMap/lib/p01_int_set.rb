253301300035fdb6b0a950b88e7d0e9285187cff
Henry <henry-pan@users.noreply.github.com>
2021-02-15 14:20:31 -0800

    Complete Phase 1

--- W5D1/HashMap/lib/p01_int_set.rb
+++ W5D1/HashMap/lib/p01_int_set.rb
@@ -1,14 +1,25 @@
 class MaxIntSet
+
+  attr_reader :store
+
   def initialize(max)
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    if !num.between?(0, @store.length-1)
+      raise "Out of bounds"
+    else
+      @store[num] = true
+    end
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
@@ -18,6 +29,7 @@ class MaxIntSet
 
   def validate!(num)
   end
+
 end
 
 
@@ -27,12 +39,17 @@ class IntSet
   end
 
   def insert(num)
+    index = num % num_buckets
+    @store[index] << num
   end
 
   def remove(num)
+    index = num % num_buckets
+    @store[index].delete(num)
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
@@ -55,12 +72,25 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if @count < num_buckets && !self.include?(num)
+      @store[num % num_buckets] << num
+      @count += 1
+    elsif @count >= num_buckets && !self.include?(num)
+      resize!
+      @store[num % num_buckets] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self.include?(num)
+      @store[num % num_buckets].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    return @store[num % num_buckets].include?(num)
   end
 
   private
@@ -74,5 +104,11 @@ class ResizingIntSet
   end
 
   def resize!
+    temp = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |ele|
+      ele.each { |num| temp[num % (num_buckets * 2)] << num}
+    end
+    @store = temp
   end
+
 end
