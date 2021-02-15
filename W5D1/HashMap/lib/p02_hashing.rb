defb5d70fbbdeb2b1f0ab18dfcc2cb7e918865b0
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-02-15 14:57:09 -0800

    Complete phase 02

--- W5D1/HashMap/lib/p02_hashing.rb
+++ W5D1/HashMap/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    self.join("").to_i.hash
   end
 end
 
 class String
   def hash
+    new_str = self.dup
+    new_str.each_char.with_index do |char, i|
+      new_str[i] = char.ord.to_s
+    end
+    new_str.to_i.hash
   end
 end
 
@@ -16,6 +22,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    new_keys = self.keys.sort
+    new_keys.to_s.hash
   end
 end
