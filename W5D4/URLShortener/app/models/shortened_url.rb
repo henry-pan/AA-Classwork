f7fb3e8313bb0296c072703475b702bf44eb09b4
Henry <henry-pan@users.noreply.github.com>
2021-02-18 16:58:19 -0800

    Fix formatting for phase 2

--- W5D4/URLShortener/app/models/shortened_url.rb
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -1,25 +1,24 @@
 require "securerandom"
 
 class ShortenedUrl < ApplicationRecord
-    validates :long_url, presence: true, uniqueness: true
-    validates :short_url, presence: true, uniqueness: true
-    
-    def self.random_code
-        random = SecureRandom.urlsafe_base64
+  validates :long_url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true
 
-        while ShortenedUrl.exists?(:short_url => random)
-            random = SecureRandom.urlsafe_base64
-        end
+  def self.random_code
+    random = SecureRandom.urlsafe_base64
 
-        random
+    while ShortenedUrl.exists?(:short_url => random)
+      random = SecureRandom.urlsafe_base64
     end
 
-    def self.create_short_url(user, long_url)
-        ShortenedUrl.create!(long_url: long_url, short_url: ShortenedUrl.random_code, user_id: user.id)
-    end
+    random
+  end
 
-    belongs_to :submitter,
+  def self.create_short_url(user, long_url)
+    ShortenedUrl.create!(long_url: long_url, short_url: ShortenedUrl.random_code, user_id: user.id)
+  end
+
+  belongs_to :submitter,
     foreign_key: :user_id,
     class_name: :User
 end
-
