d42b33d1998ec9be7ff25119730195407288167a
vickiychung <vickiychung@gmail.com>
2021-02-26 16:56:06 -0800

    Complete most of the forms

--- W6D5/app/controllers/cats_controller.rb
+++ W6D5/app/controllers/cats_controller.rb
@@ -15,9 +15,33 @@ class CatsController < ApplicationController
     render :new
   end
 
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat.id)
+    else
+      render :new
+    end
+  end
+
   def edit
     @cat = Cat.find_by(id: params[:id])
     render :edit
   end
 
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def cat_params
+      params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
 end
--- W6D5/db/seeds.rb
+++ W6D5/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Cat.destroy_all
+
 cat1 = Cat.create!(birth_date: "2015/01/20", color: "Black", name: "Bentley", sex: "M", description: "Sleepy cat.")
 cat2 = Cat.create!(birth_date: "2018/11/20", color: "Orange", name: "Bijou", sex: "F", description: "Always following Bentley.")
 cat3 = Cat.create!(birth_date: "2000/01/20", color: "White", name: "Dog", sex: "M", description: "Actually a cat.")
