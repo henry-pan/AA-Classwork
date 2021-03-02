f21a8c23a48f238164fb3e976b53a526b45c66ef
Henry <henry-pan@users.noreply.github.com>
2021-03-02 15:54:07 -0800

    Add views and controllers for Users, Bands, and Sessions

--- W7D2/app/controllers/users_controller.rb
+++ W7D2/app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-  render :new
-end
-
-def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to user_url(@user)
-  else
+  def new
+    @user = User.new
     render :new
   end
-end
 
-def show
-  @user = User.find_by(id: params[:id])
-  render :show
-end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
 
-private
-def user_params
-  params.require(:user).permit(:email, :password)
-end
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
 end
