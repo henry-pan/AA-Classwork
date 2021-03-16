fd650327fe3ada5b0dd30078c925b399c1466cc5
byoung <youngben@earthlink.net>
2021-03-16 11:56:44 -0700

    file structure in place and circle drawn

--- W9D2/src/index.js
+++ W9D2/src/index.js
@@ -1,1 +1,17 @@
-console.log("Webpack is working!");
+// console.log("Webpack is working!");
+
+const MovingObject = require("./moving_object.js");
+
+window.addEventListener ("DOMContentLoaded", function () {
+    let canvas = document.getElementById("game-canvas");
+    let ctx = canvas.getContext ("2d");
+    const mo = new MovingObject({
+        pos: [30, 30],
+        vel: [10, 10],
+        radius: 5,
+        color: "#00FF00"
+    });
+    mo.draw(ctx);
+});
+
+window.MovingObject = MovingObject;
