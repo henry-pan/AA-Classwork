384d7bc70f04596c72abfd8026bde882c19aa6d0
Henry <henry-pan@users.noreply.github.com>
2021-03-16 14:21:59 -0700

    Complete asteroid constructor

---
+++ W9D2/src/asteroid.js
@@ -0,0 +1,15 @@
+const MovingObject = require("./moving_object");
+const Util = require("./utils");
+
+const astOptions = {
+  COLOR: "#404040",
+  RADIUS: 30
+}
+
+function Asteroid (options)  {
+  MovingObject.call(this, {pos: options.pos, vel: Util.randomVec(2), color: astOptions.COLOR, radius: astOptions.RADIUS});
+}
+
+Util.inherits(Asteroid, MovingObject);
+
+module.exports = Asteroid;
--- W9D2/src/index.js
+++ W9D2/src/index.js
@@ -1,17 +1,23 @@
-// console.log("Webpack is working!");
-
 const MovingObject = require("./moving_object.js");
+const Asteroid = require("./asteroid.js");
 
 window.addEventListener ("DOMContentLoaded", function () {
-    let canvas = document.getElementById("game-canvas");
-    let ctx = canvas.getContext ("2d");
-    const mo = new MovingObject({
-        pos: [30, 30],
-        vel: [10, 10],
-        radius: 5,
-        color: "#00FF00"
-    });
-    mo.draw(ctx);
+  let canvas = document.getElementById("game-canvas");
+  let ctx = canvas.getContext ("2d");
+  const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+  });
+  mo.draw(ctx);
+  const ast = new Asteroid({ pos: [100, 100] });
+  ast.draw(ctx);
+  console.log("asteroid pos", ast.pos);
+  console.log("asteroid vel", ast.vel);
+  console.log("asteroid color", ast.color);
+  console.log("asteroid radius", ast.radius);
 });
 
 window.MovingObject = MovingObject;
+window.Asteroid = Asteroid;
