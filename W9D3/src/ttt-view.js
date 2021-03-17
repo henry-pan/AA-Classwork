c8f68241567ff50830f41120265b5a75e5df3157
Anug Saha <anugsaha@gmail.com>
2021-03-17 19:22:55 -0400

    create ttt w/ reader for winner and draw

--- W9D3/src/ttt-view.js
+++ W9D3/src/ttt-view.js
@@ -3,11 +3,39 @@ class View {
     this.game = game;
     this.$el = $el;
     this.setupBoard();
+    this.bindEvents(); 
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on("click", "li", e => { 
+      const $square= $(e.currentTarget); 
+      this.makeMove($square); 
+    }); 
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    const pos= $square.data("pos"); 
+    const currentPlayer= this.game.currentPlayer; 
+    try{ 
+      this.game.playMove(pos);
+    } catch (e){ 
+      alert ("Invalid Move, Try Again!"); 
+      return; 
+    } 
+    $square.text(currentPlayer); 
+    $square.addClass(currentPlayer); 
+    if (this.game.isOver()){  
+      this.$el.off("click");
+      const figcaption= $("<figcaption>"); 
+      if (this.game.winner()){
+        figcaption.text(`${this.game.winner()} has won!`);
+      }else{
+        figcaption.text("It's a Draw!"); 
+      }
+      this.$el.append(figcaption); 
+    } 
+
+  }
 
   setupBoard() {
     const $ul = $("<ul>");
@@ -15,6 +43,7 @@ class View {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         let $li = $("<li>");
+        $li.data("pos", [i, j]); 
         $ul.append($li);
       }
     }
