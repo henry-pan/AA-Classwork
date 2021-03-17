5d2ae5dffdb7d9ad08e7f1555f1dc1e85ad269ce
Henry <henry-pan@users.noreply.github.com>
2021-03-17 12:22:26 -0700

    Progress on setupBoard

--- W9D3/src/index.js
+++ W9D3/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+const Game = require("./game.js");
 
   $(() => {
-    // Your code here
+    const game = new Game();
+    const el = $(".ttt");
+    new View(game, el);
   });
--- W9D3/src/ttt-view.js
+++ W9D3/src/ttt-view.js
@@ -1,11 +1,26 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const $ul = $("<ul>");
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let $li = $("<li>");
+        $ul.append($li);
+      }
+    }
+
+    this.$el.append($ul);
+  }
 }
 
 module.exports = View;
