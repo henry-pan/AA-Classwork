0c6d795847fa7d2299e31e956a191cb6d8ae2fa8
Henry <henry-pan@users.noreply.github.com>
2021-03-17 17:14:28 -0700

    Complete Tic Tac Toe

--- W9D3/src/ttt-view.js
+++ W9D3/src/ttt-view.js
@@ -3,38 +3,44 @@ class View {
     this.game = game;
     this.$el = $el;
     this.setupBoard();
-    this.bindEvents(); 
+    this.bindEvents();
   }
 
   bindEvents() {
-    this.$el.on("click", "li", e => { 
-      const $square= $(e.currentTarget); 
-      this.makeMove($square); 
-    }); 
+    this.$el.on("click", "li", e => {
+      const $square = $(e.currentTarget);
+      this.makeMove($square);
+    });
   }
 
   makeMove($square) {
-    const pos= $square.data("pos"); 
-    const currentPlayer= this.game.currentPlayer; 
-    try{ 
+    const pos = $square.data("pos");
+    const currentPlayer = this.game.currentPlayer;
+    try{
       this.game.playMove(pos);
-    } catch (e){ 
-      alert ("Invalid Move, Try Again!"); 
-      return; 
-    } 
-    $square.text(currentPlayer); 
-    $square.addClass(currentPlayer); 
-    if (this.game.isOver()){  
-      this.$el.off("click");
-      const figcaption= $("<figcaption>"); 
-      if (this.game.winner()){
-        figcaption.text(`${this.game.winner()} has won!`);
-      }else{
-        figcaption.text("It's a Draw!"); 
-      }
-      this.$el.append(figcaption); 
-    } 
+    } catch (e){
+      alert ("Invalid Move, Try Again!");
+      return;
+    }
 
+    $square.text(currentPlayer);
+    $square.addClass(currentPlayer);
+    
+    if (this.game.isOver()){
+      this.$el.off("click");
+
+      $("li").addClass("gameover");
+
+      const figcaption = $("<figcaption>");
+      const winner = this.game.winner();
+      if (winner) {
+        figcaption.text(`${winner} has won!`);
+        $(`.${winner}`).addClass("winner");
+      } else {
+        figcaption.text("It's a Draw!");
+      }
+      this.$el.append(figcaption);
+    }
   }
 
   setupBoard() {
@@ -43,7 +49,7 @@ class View {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         let $li = $("<li>");
-        $li.data("pos", [i, j]); 
+        $li.data("pos", [i, j]);
         $ul.append($li);
       }
     }
