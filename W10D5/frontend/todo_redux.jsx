d3de1a30971acd4481b7ff9da95882633d7db995
Henry <henry-pan@users.noreply.github.com>
2021-03-25 15:50:21 -0700

    Complete todo_list_container

--- W10D5/frontend/todo_redux.jsx
+++ W10D5/frontend/todo_redux.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-import App from "./components/app";
+import Root from "./components/root";
+import { allTodos } from "./reducers/selectors";
 import { receiveTodos, receiveTodo, removeTodo } from "./actions/todo_actions";
 import { receiveSteps, receiveStep, removeStep } from "./actions/step_actions";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const content = document.getElementById("content");
-  ReactDOM.render(<App />,content);
-
   const store = configureStore();
   window.store = store;
   window.receiveTodos = receiveTodos;
@@ -18,4 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
   window.receiveSteps = receiveSteps;
   window.receiveStep = receiveStep;
   window.removeStep = removeStep;
+  window.allTodos = allTodos;
+
+  const content = document.getElementById("content");
+  ReactDOM.render(<Root store={store}/>,content);
 });
