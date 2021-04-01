6f4c93f0fdb0838663648b853fdf89a4ba7ff363
Henry <henry-pan@users.noreply.github.com>
2021-04-01 15:41:23 -0700

    Add reducers

---
+++ W11D4/benchbnb/frontend/actions/session_actions.js
@@ -0,0 +1,31 @@
+import { signup, login, logout } from "../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const login = user => dispatch => login(user)
+  .then(user => dispatch(receiveCurrentUser(user)))
+  .catch(errors => dispatch(receiveErrors(errors)));
+
+export const logout = () => dispatch => logout()
+  .then(() => dispatch(logoutCurrentUser()))
+  .catch(errors => dispatch(receiveErrors(errors)));
+
+export const signup = user => dispatch => signup(user)
+  .then(user => dispatch(receiveCurrentUser(user)))
+  .catch(errors => dispatch(receiveErrors(errors)));
